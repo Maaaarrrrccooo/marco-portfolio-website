e15f5c28a8d17224bcd60fc9d862c41c1457d4e8
Marco Bustillo <marcobustillo21@gmail.com>
2023-03-08 18:29:27 +0800

    refactor required providers

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -5,12 +5,11 @@ terraform {
       version = ">= 3.00.0"
     }
 
-    required_providers {
-      cloudflare = {
-        source  = "cloudflare/cloudflare"
-        version = ">= 3.0"
-      }
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = ">= 3.0"
     }
+
   }
 
   backend "s3" {}
