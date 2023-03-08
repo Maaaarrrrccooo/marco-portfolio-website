d97086f67b75a33143464c53b119f8599007f586
Marco Bustillo <marcobustillo21@gmail.com>
2023-03-08 15:45:49 +0800

    Update versions

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,9 +1,8 @@
 terraform {
-  required_version = "1.0.7"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.88.0"
+      version = ">= 3.00.0"
     }
   }
 
